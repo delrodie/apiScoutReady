76570ea2f8285b7d5b41920d067e116024b7b32e
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-07-29 06:55:43 +0000

    hotfix/apiScoutPatchController Groupe v1.2.3

--- src/Controller/ApiScoutPatchController.php
+++ src/Controller/ApiScoutPatchController.php
@@ -15,12 +15,19 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use function Symfony\Component\Clock\now;
 
 class ApiScoutPatchController extends AbstractController
 {
-    public function __construct(private readonly AllRepositories $allRepositories, private readonly Gestion $gestion, private readonly GestionMedia $gestionMedia, private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly AllRepositories $allRepositories,
+        private readonly Gestion $gestion,
+        private readonly GestionMedia $gestionMedia,
+        private readonly EntityManagerInterface $entityManager
+    )
     {
     }
 
@@ -31,18 +38,22 @@ class ApiScoutPatchController extends AbstractController
         SerializerInterface $serializer,
     ): JsonResponse|ScoutOutput
     {
-
+        // Récuperons le scout existant
        $reqScout = $this->allRepositories->getOneScout((int) $id);
        if (!$reqScout){
            return new JsonResponse(['error' => "Scout non trouvé"], 404);
        }
 
+       // Assignons les données de la requête au scout
        $scout = $this->assignRequestData($request, $reqScout);
 
+       // Traitons la photo uploadée
         $this->processUploadedPhoto($request, $scout); //dd($scout);
 
+        // persistons les changements
         $this->entityManager->flush();
 
+        // Retournons une réponse de succès
         return new JsonResponse([
             'statut' => "Mise à jour effectuée avec succès!"
         ], 200);
@@ -50,52 +61,78 @@ class ApiScoutPatchController extends AbstractController
 
     private function assignRequestData($request, object $scout): object
     {
-        if ($matricule = $request->get('matricule')){
+        // Assurons-nous que $Scout est bien une instance d'App\Entity\Scout
+        if(!$scout instanceof Scout){
+            throw new \InvalidArgumentException("L'objet scout doit être une instance de App\entity\Scout");
+        }
+
+        if ($request->request->has('matricule')){
+            $matricule = $request->request->get('matricule');
             $scout->setMatricule(strtoupper($this->gestion->validForm($matricule)));
         }
 
-        if ($nom = $request->get('nom')){
+        if ($request->request->has('nom')){
+            $nom = $request->request->get('nom');
             $scout->setNom(strtoupper($this->gestion->validForm($nom)));
         }
 
-        if ($prenom = $request->get('prenom')){
+        if ($request->request->has('prenom')){
+            $prenom = $request->request->get('prenom');
             $scout->setPrenom(strtoupper($this->gestion->validForm($prenom)));
         }
 
-        if ($sexe = $request->get('sexe')){
+        if ($request->request->has('sexe')){
+            $sexe = $request->request->get('sexe');
             $scout->setSexe(strtoupper($this->gestion->validForm($sexe)));
         }
 
-        if ($dateNaissance = $request->get('dateNaissance')){
+        if ($request->request->has('dateNaissance')){
+            $dateNaissance = $request->request->get('dateNaissance');
             $scout->setDateNaissance($this->parseDate($dateNaissance));
         }
 
-        if ($lieuNaissance = $request->get('lieuNaissance')){
+        if ($request->request->has('lieuNaissance')){
+            $lieuNaissance = $request->request->get('lieuNaissance');
             $scout->setLieuNaissance($lieuNaissance);
         }
 
-        if ($telephone = $request->get('telephone')){
+        if ($request->request->has('telephone')){
+            $telephone = $request->request->get('telephone');
             $scout->setTelephone($telephone);
         }
 
-        if ($email = $request->get('email')){
+        if ($request->request->has('email')){
+            $email = $request->request->get('email');
             $scout->setEmail($email);
         }
 
-        if ($fonction = $request->get('fonction')){
+        if ($request->request->has('fonction')){
+            $fonction = $request->request->get('fonction');
             $scout->setFonction($fonction);
         }
 
-        if ($branche = $request->get('branche')){
+        if ($request->request->has('branche')){
+            $branche = $request->request->get('branche');
             $scout->setBranche($branche);
         }
 
-        if ($statut = $request->get('statut')){
+        if ($request->request->has('statut')){
+            $statut = $request->request->get('statut');
             $scout->setStatut($statut);
         }
 
-        if ($telephoneParent = $request->get('telephoneParent')){
-            $scout->setTelephoneParent($telephoneParent);
+        if ($request->request->has('telephoneParent')){
+            $telephoneParent = $request->get('telephoneParent');
+            $scout->setTelephoneParent(filter_var($telephoneParent, FILTER_VALIDATE_BOOLEAN));
+        }
+
+        if($request->request->has('groupe')){
+            $groupeId = (int) $request->request->get('groupe');
+            $groupe = $this->allRepositories->getOneGroupe($groupeId);
+            if (!$groupe){
+                throw new NotFoundHttpException("Le groupe avec l'ID {$groupeId} n'a pas été trouvé");
+            }
+            $scout->setGroupe($groupe);
         }
         
         return $scout;
